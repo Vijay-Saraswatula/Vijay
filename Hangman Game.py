fda1b19086afcd06bf66607d75d38353dd5d67a3
Vijay-Saraswatula <76033651+Vijay-Saraswatula@users.noreply.github.com>
2021-12-02 18:46:48 +0530

    Update Hangman Game.py

--- Hangman Game.py
+++ Hangman Game.py
@@ -1,23 +1,21 @@
 word_list = ["aardvark", "banana", "baboon", "camel"]
 import random
-a=random.choice(word_list) #random word
-b=len(a) #b = length of the word
-c=[]
-d=[]
+a = random.choice(word_list)
+b, c, d = len(a) [], []
 for x in range(0,b):
-  c.append(a[x]) #Word is broken into letters
+  c.append(a[x])
   d.append("_")
 print(f"You only have {b+1} lifes.")
 for x in range(0,b+1):
-  if d.count("_")==0:
+  if d.count("_") == 0:
     print("\n   You win!")
     exit()
-  i=input("\nGuess a letter: ").lower()
+  i = input("\nGuess a letter: ").lower()
   for k in range(0,b):
-    if c[k]==i:
-      d[k]=i
+    if c[k] == i:
+      d[k] = i
     print(d[k],end=" ")
-if d.count("_")!=0:
+if d.count("_") != 0:
   print("\nOut of lives!\n    Game Over!")
 else:
   print("\n   You win!")
