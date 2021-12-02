752713637b859906dcedb43235bc4e329a5fbcab
Vijay-Saraswatula <76033651+Vijay-Saraswatula@users.noreply.github.com>
2021-12-02 18:50:31 +0530

    Update Musical Chair Game.py

--- Musical Chair Game.py
+++ Musical Chair Game.py
@@ -1,12 +1,11 @@
-#Musical Chairs
-n=int(input("Enter number of chairs: "))
+n = int(input("Enter number of chairs: "))
 print("Enter Chair Numbers:")
-arr=[]
-lis=[]
+arr = []
+lis = []
 for i in range(0,n):
   arr.append(int(input()))
-c=arr.copy()
-x=int(input("Enter the min. chair number value: "))
+c = arr.copy()
+x = int(input("Enter the min. chair number value: "))
 for i in range(0,n):
   for j in range(0,n):
     if arr[j]<=x:
@@ -16,5 +15,4 @@ for i in range(0,n):
       arr.append(arr[j]-x)
       arr.pop(j)
       lis.append(c[j])
-l=len(lis)
-print(f"Chair number {lis[l-1]} is the winner")
+print(f"Chair number {lis[len(lis)-1]} is the winner")
